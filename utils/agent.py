8cbb773ae1b56b7d2a9740bae09f7d2f0cafa19d
ChandrahaasJasti <chandrahaas.intern@saarathifinance.com>
2025-08-13 18:12:36 +0530

    agent

--- utils/agent.py
+++ utils/agent.py
@@ -1,5 +1,6 @@
 from .rag import EmbRag
 from .auth import LLM
+ENV_PATH=r"/home/chandrahaas/codes/Bot/.env"
 DOCS_PATH=r"/home/chandrahaas/codes/Bot/DOCS"
 FAISS_PATH=r"/home/chandrahaas/codes/Bot/FAISS"
 
@@ -7,3 +8,13 @@ FAISS_PATH=r"/home/chandrahaas/codes/Bot/FAISS"
 class Agent:
     def __init__(self):
         self.rag=EmbRag(DOCS_PATH,FAISS_PATH)
+        self.llm=LLM(ENV_PATH)
+
+    def start_agent(self,user_query,screen_shot=True,image_base64=None):
+        if(screen_shot):
+            perception=self.get_screen_perception(user_query,image_base64)
+        else:
+            perception=self.get_perception(user_query)
+        decision=self.get_decision(perception)
+        json_decision=eval(decision)
+        if(json_decision["agent"]=="executer"):
